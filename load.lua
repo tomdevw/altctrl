9b7f605027a848bf311caea10c550080a96376b3
tomdevw <tomdevw@gmail.com>
2024-04-03 13:38:29 +0100

    aqa

--- load.lua
+++ load.lua
@@ -2,7 +2,8 @@ getgenv().prefix = '.' -- The prefix for the commands example: :fly username
 getgenv().controller = 604950457-- Enter controllers userid! Make sure the id isnt in the alts
 getgenv().altFPS = 5 -- set the FPS for alts
 getgenv().alts = { 
-    Alt1 = 5602254951
+    Alt1 = 5602254951,
+    Alt2 = 4578675842,
 }
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/tomdevw/qq/main/aa.lua', true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/tomdevw/we4fewefwefwefwefwefwefwe/main/qqqqqqqwqw2w2dqwwdqwdqwd.lua', true))()\
